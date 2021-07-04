c390ae60bb99e223f176b6f87ea8827a7a9ba8f8
LABMATT <matt42764@gmail.com>
2021-07-04 22:53:23 +1000

    ssocketio

--- src/main/java/socketManger/SocketIoServlet.java
+++ src/main/java/socketManger/SocketIoServlet.java
@@ -1,9 +1,15 @@
 package socketManger;
 
+import io.socket.emitter.Emitter;
 import io.socket.engineio.server.EngineIoServer;
 import io.socket.socketio.server.SocketIoServer;
+import io.socket.socketio.server.SocketIoSocket;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
 
 import java.io.IOException;
+
 
 @WebServlet("/socket.io/*")
 public class SocketIoServlet extends HttpServlet {
@@ -15,4 +21,12 @@ public class SocketIoServlet extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
         mEngineIoServer.handleRequest(request, response);
     }
+
+    this.on("connection", new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            SocketIoSocket socket = (SocketIoSocket) args[0];
+            // Do something with socket
+        }
+    });
 }
